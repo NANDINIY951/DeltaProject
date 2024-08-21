9dedf01d878b2857d208b1a5b8563b2d5bcecc4d
nandini <nandiniy617@gmail.com>
2024-08-22 00:02:56 +0530

    modified app.js (ejs)

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const passport = require('passport');
 const LocalStrategy = require("passport-local");
 const userRoute = require("./routes/userRoute.js");
 
-app.set("view engine","EJS");
+app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"/views"));
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
